ecdeabe78574eed05df991b25282eebb806aebe9
MARIO <151770891+benedictguymario@users.noreply.github.com>
2025-01-30 22:00:08 +0000

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,33 +1,3 @@
-# import streamlit as st
-# import requests
-
-# st.set_page_config(page_title="Athentification",page_icon=":lock:",layout="centered",initial_sidebar_state="expanded")
-# st.title(':lock: Bienvenue sur la page d\'authentification')
-
-# choix = st.radio("que voulez vous faire ?",["Se connecter","S'inscrire"])
-
-# if choix == "Se connecter":
-#     if st.button("Se connecter"):
-#         st.success("connectez-vous")
-#         st.markdown("<meta http-equiv='refresh' content='1;URL=http://localhost:8000'>",unsafe_allow_html=True)
-# elif choix == "S'inscrire":
-#     st.subheader(":pencil: Créer un compte")
-#     with st.form("register"):
-#         userEmail = st.text_input("mail de l'utilisateur")
-#         password = st.text_input("Mot de passe", type="password")
-#         submit = st.form_submit_button("S'inscrire")
-#         if submit:
-#             if userEmail and password:
-#                 response = requests.post("http://localhost:5000/register", data={"userEmail": userEmail, "password": password})
-#                 if response.status_code == 303:
-#                     st.success("Inscription réussie ! Vous pouvez maintenant vous connecter.")
-#                     st.markdown("<meta http-equiv='refresh' content='1;URL=http://localhost:8000'>",unsafe_allow_html=True)
-#                 else:
-#                     st.error("utilisateur exist deja, essayez un autre.")
-#             else:
-#                 st.warning(" Veuillez remplir tous les champs")
-
-
 import streamlit as st
 import requests
 
@@ -51,11 +21,11 @@ elif choix == "S'inscrire":
 
         if submit:
             if userEmail and password:
-                response = requests.post("http://localhost:5000/register", data={"userEmail": userEmail, "password": password})
+                response = requests.post("https://d354.onrender.com", data={"userEmail": userEmail, "password": password})
 
                 if response.status_code == 201:  # Inscription réussie
                     st.success("Inscription réussie ! Vous allez être redirigé...")
-                    st.markdown("<meta http-equiv='refresh' content='2;URL=http://localhost:8000'>", unsafe_allow_html=True)
+                    st.markdown("<meta http-equiv='refresh' content='2;URL=https://chatecofin.onrender.com'>", unsafe_allow_html=True)
                 elif response.status_code == 400:  # Utilisateur déjà existant
                     st.error("L'utilisateur existe déjà, essayez un autre email.")
                 else:
